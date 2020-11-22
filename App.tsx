3ae49322caad0c19d2b25ea21e919e6033f4e8c7
米倉謙 <p.p.high.llows@gmail.com>
2020-11-22 10:34:36 +0900

    モジュールの宣言ファイルを作成

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 //App.tsx
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import logo from "./assets/sparta.jpg";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: "https://bit.ly/2ULtog9" }} style={styles.logo} />
+      <Image source={ logo } style={styles.logo} />
       <Text style={styles.instructions}>Hello, Sparta Camp!</Text>
     </View>
   );
