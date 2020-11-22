edd819813ebda699f5fa6767904018be2c9749b1
米倉謙 <p.p.high.llows@gmail.com>
2020-11-22 14:07:20 +0900

    画像表示まで完了

--- App.tsx
+++ App.tsx
@@ -3,16 +3,52 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import logo from "./assets/sparta.jpg";
+import * as ImagePicker from7 "expo-image-picker";
 
 export default function App() {
+
+  interface SelectedImageInfo{
+    localUri: String;
+  }
+
+  const [selectedImage, setSelectedImage] = React.useState<SelectedImageInfo | undefined>();
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    // launchImageLibraryAsync();はカメラロールを事前に準備しておくよ｡というお作法
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+
+    if (pickerResult.cancelled === true) {
+    } else {
+      const selectedUri = {};
+      console.log(pickerResult);
+      setSelectedImage({ localUri: pickerResult.uri });
+    }
+
+    if (permissionResult.granted === false) {
+      alert("カメラロールへのアクセス許可が必要です");
+      return;
+    }
+  };
+
+  if (selectedImage !== (null || undefined)){
+    return(
+      <View style={styles.container}>
+        <Image
+          source={{uri: selectedImage?.localUri}}
+          style={styles.thumbnail}
+        />
+      </View>
+    )
+  }
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
       <Text style={styles.instructions}>Hello, Sparta Camp!</Text>
-      <Pressable 
-      onPress={() => alert("Hello World!")}
-      style={{backgroundColor:"blue"}}>
-        <text style={{fontSize: 20, color:"#fff"}}>Pick a Photo</text>
+      <Pressable onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
       </Pressable>
     </View>
   );
@@ -35,5 +71,19 @@ const styles = StyleSheet.create({
     color: "magenta",
     fontSize: 30,
     marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
   },
 });
